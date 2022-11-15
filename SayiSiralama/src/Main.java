7fdb922591176f011b9c9138f448b9dd0b6837c3
Üveys Saraç <uveys.sarac@stu.fsm.edu.tr>
2022-11-15 16:17:23 +0300

    add

---
+++ SayiSiralama/src/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner inp = new Scanner(System.in);
+        System.out.println("a sayısını giriniz : "); // a sayıyını kullanıcıdan alma
+        int a = inp.nextInt();
+        System.out.println("b sayısını giriniz : "); // b sayıyını  kullanıcıdan alma
+        int b = inp.nextInt();
+        System.out.println("c sayısını giriniz : "); // c sayıyını kullanıcıdan alma
+        int c = inp.nextInt();
+        String result="";
+
+        if(a>b && a>c){             //En büyük a ise result stringine a eklenir.
+            result+="a";
+            if(b>c){               //b>c b ise result stringine a eklenir.Değilse sıralamaya c>b diye eklenir
+                result+=">b>c";
+            }else{
+                result+=">c>b";
+            }
+        } else if (b>a && b>c) {//En büyük b ise result stringine a eklenir.
+            result+="b";
+            if(a>c){
+                result+=">a>c";     //a>c b ise result stringine a eklenir.Değilse sıralamaya c>a diye eklenir
+            }else{
+                result+=">c>a";
+            }
+        } else{                 //En büyük c ise result stringine a eklenir.
+            result+="c";
+            if(b>a){                //b>a b ise result stringine a eklenir.Değilse sıralamaya a>b diye eklenir
+                result+=">b>a";
+            }else{
+                result+=">a>b";
+            }
+        }
+        System.out.println(result); //Sonda da result yazdırılır
+
+    }
+}
