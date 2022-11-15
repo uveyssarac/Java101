b6838ccd89e5f464d36ca42142c6a0fcd86889a3
Üveys Saraç <uveys.sarac@stu.fsm.edu.tr>
2022-11-15 14:53:24 +0300

    add

--- KullaniciGirisi/src/Main.java
+++ KullaniciGirisi/src/Main.java
@@ -1,4 +1,3 @@
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
@@ -12,24 +11,25 @@ public class Main {
         System.out.println("Şifrenizi giriniz : "); // Şifreyi kullanıcıdan alma
         password = inp.nextLine();
 
-        if(userName.equals(correctUsername) && password.equals(correctPassword)){
-            System.out.println("Giriş yaptınız");
-        }else if(!(password.equals("dev"))){
+        if(userName.equals(correctUsername) && password.equals(correctPassword)){   // Kullanıcı adı ve şifre kontrolü
+            System.out.println("Giriş yaptınız");        // Kullanıcı adı ve şifre doğruysa giriş yapılır.
+        }else if(!(password.equals("dev"))){             // Hatalı şifre kontrolü
             System.out.println("Şifreniz hatalıdır.\nŞifrenizi değiştirmek ister misiniz? evet/hayır ");
-            String result = inp.nextLine();
-            if(result.equals("evet")){
+            String result = inp.nextLine(); //Şifreyi değiştirmek istediği sorulur
+
+            if(result.equals("evet")){              // Değiştirmek istiyorsa eski şifre ile aynı olup olmadığı kontrol edilir
                 System.out.println("Yeni şifrenizi giriniz.");
                 String newPassword = inp.nextLine();
                 if(newPassword.equals(correctPassword)){
                     System.out.println("Eski şifreniz ile yeni şifreniz aynı olamaz :)");
-                }else{
+                }else{              // Eski şifre ile yeni şifre aynı değil ise değiştirilir
                     correctPassword=newPassword;
                     System.out.println("Yeni şifre başarıyla oluşturuldu");
                 }
             }else{
                 System.out.println("Şifre değiştirilmek istenmedi.");
             }
-        }else {
+        }else {     // Kullanıcı adı ve şifre hatalıysa ayrıca şifreyi de değiştirmek istemiyorsa bu sonuç döndürülür
             System.out.println("Kullanıcı adı veya şifre yanlış olduğu için giriş yapılamadı.");
         }
     }
