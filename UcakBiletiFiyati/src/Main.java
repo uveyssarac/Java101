d08164fbc32c070d5e8cdc6a59d0e3c6266c15c2
Üveys Saraç <uveys.sarac@stu.fsm.edu.tr>
2022-11-16 12:52:03 +0300

    add

--- UcakBiletiFiyati/src/Main.java
+++ UcakBiletiFiyati/src/Main.java
@@ -5,25 +5,25 @@ public class Main {
 
         Scanner inp = new Scanner(System.in);
         System.out.println("Mesafe (KM) : ");
-        int distance = inp.nextInt();           // Mesafe bilgisini alma
-        if(!(distance>0)){                      // Mesafe pozitif değer kontrolü
+        int distance = inp.nextInt();               // Mesafe bilgisini alma
+        if(!(distance>0)){                          // Mesafe pozitif değer kontrolü
             System.out.println("Hatalı veri girdiniz");
-            System.exit(0);
+            System.exit(0);                   //Veri hatalı ise program sonlanır
         }
 
-        System.out.println("Yaşınızı giriniz : "); // Yaş bilgisini alma
+        System.out.println("Yaşınızı giriniz : ");  // Yaş bilgisini alma
         int age = inp.nextInt();                    // Yaş pozitif değer kontrolü
         if(!(age>0)){
             System.out.println("Hatalı veri girdiniz");
-            System.exit(0);
+            System.exit(0);                    //Veri hatalı ise program sonlanır
         }
         System.out.println("Tek yön için : 1, Gidiş dönüş için : 2, değerini giriniz  : ");
         int trip = inp.nextInt();
-        if(!(age==1 || age ==2)){
+        if(!(age==1 || age ==2)){                    //Yolculuk tipi kontrolü
             System.out.println("Hatalı veri girdiniz");
-            System.exit(0);
+            System.exit(0);                    //Veri hatalı ise program sonlanır
         }
-        float kmPrice=0.10f; //Km başına ücret
+        float kmPrice=0.10f;                         //Km başına ücret
 
         System.out.println("Normal tutar :"+distance*kmPrice*trip);
 
