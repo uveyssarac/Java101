5f528652d3e3de5319850c3d6796499571cd9db8
Üveys Saraç <uveys.sarac@stu.fsm.edu.tr>
2022-11-16 12:45:26 +0300

    add

--- UcakBiletiFiyati/src/Main.java
+++ UcakBiletiFiyati/src/Main.java
@@ -6,16 +6,23 @@ public class Main {
         Scanner inp = new Scanner(System.in);
         System.out.println("Mesafe (KM) : ");
         int distance = inp.nextInt();           // Mesafe bilgisini alma
+        if(!(distance>0)){                      // Mesafe pozitif değer kontrolü
+            System.out.println("Hatalı veri girdiniz");
+            System.exit(0);
+        }
 
         System.out.println("Yaşınızı giriniz : "); // Yaş bilgisini alma
-        int age = inp.nextInt();
-
+        int age = inp.nextInt();                    // Yaş pozitif değer kontrolü
+        if(!(age>0)){
+            System.out.println("Hatalı veri girdiniz");
+            System.exit(0);
+        }
         System.out.println("Tek yön için : 1, Gidiş dönüş için : 2, değerini giriniz  : ");
         int trip = inp.nextInt();
 
-        float kmPrice=0.10f;
+        float kmPrice=0.10f; //Km başına ücret
 
-        System.out.println("Normal tutar :"+distance*kmPrice);
+        System.out.println("Normal tutar :"+distance*kmPrice*trip);
 
     }
 }
