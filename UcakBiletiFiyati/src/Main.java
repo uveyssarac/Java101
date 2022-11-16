8aa9613ea3e67c752d06f977f3461c068801ed1d
Üveys Saraç <uveys.sarac@stu.fsm.edu.tr>
2022-11-16 15:25:11 +0300

    add

--- UcakBiletiFiyati/src/Main.java
+++ UcakBiletiFiyati/src/Main.java
@@ -5,36 +5,40 @@ public class Main {
 
         Scanner inp = new Scanner(System.in);
         System.out.println("Mesafe (KM) : ");
+
         int distance = inp.nextInt();               // Mesafe bilgisini alma
         if(!(distance>0)){                          // Mesafe pozitif değer kontrolü
             System.out.println("Hatalı veri girdiniz");
             System.exit(0);                   //Veri hatalı ise program sonlanır
         }
+
         System.out.println("Yaşınızı giriniz : ");  // Yaş bilgisini alma
         int age = inp.nextInt();                    // Yaş pozitif değer kontrolü
         if(!(age>0)){
             System.out.println("Hatalı veri girdiniz");
             System.exit(0);                    //Veri hatalı ise program sonlanır
         }
+
         System.out.println("Tek yön için : 1, Gidiş dönüş için : 2, değerini giriniz  : ");
         int trip = inp.nextInt();
-        if(!(trip==1 || trip ==2)){                    //Yolculuk tipi kontrolü
+        if(!(trip==1 || trip ==2)){                   //Yolculuk tipi kontrolü
             System.out.println("Hatalı veri girdiniz");
             System.exit(0);                    //Veri hatalı ise program sonlanır
         }
+
         float kmPrice=0.10f;                         //Km başına ücret
         float tutar = distance*kmPrice;              //Normal ücret tutarı
-        //float ageDiscount=0;
 
+        System.out.println("İndirimsiz Tutar"+ tutar);
         if(age<12){                                  //Yaş indirimi hesaplama
             tutar=tutar-(tutar/2f);
-            System.out.println("yaş indirimi : "+tutar*0.50f);
+            System.out.println("Yaş indirimi : "+tutar*0.50f);
         } else if (age<24) {
             tutar=tutar-(tutar*0.10f);
-            System.out.println("yaş indirimi : "+tutar*0.10f);
+            System.out.println("Yaş indirimi : "+tutar*0.10f);
         } else if (age>65) {
             tutar=tutar-(tutar*0.30f);
-            System.out.println("yaş indirimi : "+tutar*0.30f);
+            System.out.println("Yaş indirimi : "+tutar*0.30f);
         }else{
             System.out.println("yaş indirimi : 0 ");
         }
