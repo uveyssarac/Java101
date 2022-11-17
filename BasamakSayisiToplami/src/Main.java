fe25dd31302b2159c9bb98e434e8be0a6fd1bdfa
Üveys Saraç <uveys.sarac@stu.fsm.edu.tr>
2022-11-17 16:30:56 +0300

    add

---
+++ BasamakSayisiToplami/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Taban değerini giriniz : ");         // data from user
+        int number = scan.nextInt();
+        int sum=0;
+        while(number >= 1){
+
+            int lastNumber=number%10;
+            sum+=lastNumber;
+            number/=10;             //her defasında sayıyı 10'a bölüp sayıyın son basamağını atmış oluyoruz
+        }
+        System.out.println(sum);
+    }
+}
